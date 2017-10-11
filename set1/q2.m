7ade79a9e88bd2ef503a4e5f9cc6e41dc9ac2908
Abin Thomas <abinthomasonline@gmail.com>
2017-10-11 20:33:08 +0530

    Update q2.m

--- set1/q2.m
+++ set1/q2.m
@@ -1,4 +1,4 @@
 beta = (0:0.01:4)'; %w/wn (x-axis)
-zeta = [0.01; 0.02; 0.05; 0.7]'; %damping ratio
-DAF = 1./sqrt((1-beta.^2).^2 + (2*beta*zeta).^2); %dynamic amplification factor (y-axis)
+zeta = [0.01, 0.02, 0.05, 0.7]; %damping ratio
+DAF = 1./sqrt((1-beta.^2).^2*ones(1,length(zeta)) + (2*beta*zeta).^2); %dynamic amplification factor (y-axis)
 plot(beta, DAF); %plot
