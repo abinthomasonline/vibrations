083d660629ca158bd7a7c65cb01e0549044be4dc
Abin Thomas <abinthomasonline@gmail.com>
2017-10-11 20:32:38 +0530

    Update q1.m

--- set1/q1.m
+++ set1/q1.m
@@ -4,11 +4,18 @@ zeta = 0.02; %damping ratio
 F = 1; %magnitude of exciting force
 wd = 2*pi; %angular frequency of exciting force
 wn = sqrt(k/m); %natural frequency
+w0 = wn*sqrt(1-zeta^2); %angular frequency of transient state
+t = 0:0.001:70;
+
 X_steady = F/(m*sqrt((wn^2-wd^2)^2+(2*wd*wn*zeta)^2)); %max amplitude of steady response
-Theta_steady = atan((2*wd*wn*zeta)/(wn^2-wd^2)); %phase angle of steady response
-Theta_transient = atan((-1*(wn*zeta+wd*tan(Theta_steady)))/(wn*sqrt(1-zeta^2))); %max amplitude of transient response
-X_transient = -1*X_steady*cos(Theta_steady)/cos(Theta_transient); %phase angle of transient response 
-t = 0:0.01:100; %time (x-axis)
-Xs = X_steady*cos(wd*t-Theta_steady); %steady state response (y-axis)
-Xt = exp(-1*wn*zeta*t).*X_transient.*cos(wn*sqrt(1-zeta^2)*t + Theta_transient); %transient state response (y-axis)
-plot(t,Xt,t,Xs); %Plots
+Theta_steady = atan((-2*wd*wn*zeta)/(wn^2-wd^2)); %phase angle of steady response
+Xs = X_steady*cos(wd*t - Theta_steady); %steady state response
+
+X0 = 0.0003; % initial displacement
+U0 = 0; % initial velocity
+
+Theta_transient = atan((1-U0/X0)/w0); %phase angle of transient response
+X_transient = X0/cos(Theta_transient); %max amplitude of transient response
+Xt = exp(-1*wn*zeta*t).*(X_transient*cos(w0*t - Theta_transient)); %transient state response
+
+plot(t,Xt,t,Xs); %plot
