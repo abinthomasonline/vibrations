e943f55dfe6c94cd480adf1b90db2220df6adcd6
Abin Thomas <abinthomasonline@gmail.com>
2017-10-22 10:58:23 +0530

    Create q1.m

---
+++ set2/q1.m
@@ -0,0 +1,33 @@
+k = 400;
+m = 100;
+zetam = [0.02, 0.05, 0.1]; %damping ratio matrix [1x3]
+wn = sqrt(k/m); %natural frequency
+load elcentro.mat %load exciting force data (file should be in the same folder)
+
+hold on; %to plot 3 responses on single graph
+
+for zeta=zetam
+	%newmark's beta method
+	c = 2*m*wn*zeta; %damping coefficient
+	ga = 0.5; %gamma
+	be = 0.25; %beta
+	dt = t(2)-t(1); %time interval
+	C1 = m/(be*dt) + (c*ga)/be; %coefficient of velocity in incrementation equation
+	C2 = m/(2*be) + (c*dt)*(ga/(2*be)-1); %coefficient of acceleration in incrementation equation
+	kc = k + (c*ga)/(be*dt) + m/(be*dt*dt); %coefficient of change in displacement in incrementation equation
+	x(1)=0; %initial displacement
+	u(1)=0; %initial velocity
+	a(1)=(p(1)-k*x(1)-c*u(1))/m; %initial acceleration
+	for i=1:1:(length(t)-1)
+		dp=p(i+1)-p(i); %change in excitation
+		dx=(dp+C1*u(i)+C2*a(i))/kc; %change in displacement
+		du=(dx*ga)/(be*dt) - (u(i)*ga)/be - a(i)*dt*(ga/(2*be)-1); %change in velocity
+		da=dx/(be*dt*dt) - u(i)/(be*dt) - a(i)/(2*be); %change in acceleration
+		x(i+1)=x(i)+dx; %increment displacement
+		u(i+1)=u(i)+du; %increment velocity
+		a(i+1)=a(i)+da; %increment acceleration
+	end
+	plot(t,x); %plot
+end
+
+hold off;
