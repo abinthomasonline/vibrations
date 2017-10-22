e32ba72e71da21b5ffff7ea5142c0ba387410e68
Abin Thomas <abinthomasonline@gmail.com>
2017-10-22 11:56:59 +0530

    Update q3.m

--- set2/q3.m
+++ set2/q3.m
@@ -6,8 +6,10 @@ for i=1:1:length(p) %right shifted multipplication
 	AC(i:i+length(p)-1) = AC(i:i+length(p)-1) + p.*p(length(p)-i+1);
 end
 
-for i=1:1:length(p) %dividing with frequency
+for i=1:1:length(p)-1 %dividing with frequency
 	AC(length(AC)-i+1) = AC(length(AC)-i+1)/i;
+	AC(i) = AC(i)/i;
 end
+AC(length(p)) = AC(length(p))/length(p);
 
-plot(t,AC(length(p):end)); %plot
+plot([-1.*flip(t),t(2:end)],AC); %plot
