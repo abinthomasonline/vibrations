d27c8ea47332f05479512064a978157b1a47558a
Abin Thomas <abinthomasonline@gmail.com>
2017-10-26 22:08:15 +0530

    Update q4a.m

--- set2/q4a.m
+++ set2/q4a.m
@@ -1,6 +1,6 @@
 load elcentro.mat %load elcentro data
 
-AC = xcov(p, length(p)-1); %autocorrelation
+AC = xcorr(p); %autocorrelation
 
 S = abs(fft(AC)); %absolute value of discrete fourier transform of AC
 
