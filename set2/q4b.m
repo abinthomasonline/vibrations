73475355148256371ff595549c13b7245f2878b4
Abin Thomas <abinthomasonline@gmail.com>
2017-11-01 00:31:54 +0530

    Update q4b.m

--- set2/q4b.m
+++ set2/q4b.m
@@ -2,7 +2,7 @@ load elcentro.mat %load elcentro data
 
 dt = t(2)-t(1); f = 1/dt;
 
-S = abs(fft(p)).^2; %absolute value of discrete fourier transform of AC
+S = abs(fft(p)).^2; %square of discrete fourier transform of p
 
 w = -f/2:f/length(p):f/2-f/length(p); %frequency axis
 
