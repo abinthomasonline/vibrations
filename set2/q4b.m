0188ac274b5ab3d21de391bf20a5ee3a0103c7fa
Abin Thomas <abinthomasonline@gmail.com>
2017-10-31 21:52:50 +0530

    Update q4b.m

--- set2/q4b.m
+++ set2/q4b.m
@@ -2,6 +2,8 @@ load elcentro.mat %load elcentro data
 
 S = (abs(fft(p)).^2).*(t(end)/(4*pi));
 
-w = lag.*((2*pi)/(length(AC)*dt)); %angular frequency axis
+dt = t(2)-t(1);
+
+w = (1-length(p):2:length(p)-1).*((2*pi)/(length(AC)*dt)); %angular frequency axis
 
 plot(w, S);
