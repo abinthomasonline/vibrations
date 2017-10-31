c5f2ebc43144993d4549075315be55b6e1c392fc
Abin Thomas <abinthomasonline@gmail.com>
2017-10-31 21:51:21 +0530

    Update q4a.m

--- set2/q4a.m
+++ set2/q4a.m
@@ -2,6 +2,8 @@ load elcentro.mat %load elcentro data
 
 [AC, lag] = xcorr(p); %autocorrelation
 
+dt = t(2)-t(1);
+
 S = abs(fft(AC)); %absolute value of discrete fourier transform of AC
 
 w = lag.*((2*pi)/(length(AC)*dt)); %angular frequency axis
