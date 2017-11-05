20801c4bd37dc5977c32c1242508e425dffadedf
Abin Thomas <abinthomasonline@gmail.com>
2017-11-05 07:39:15 +0000

    Update q3.m

--- set2/q3.m
+++ set2/q3.m
@@ -1,7 +1,17 @@
-load elcentro.mat %load elcentro data
+load elcentro.mat
 
-dt = t(2)-t(1);
+lag = 0:length(t)-1;
+auto_correlation = zeros(1,2*length(lag)-1);
 
-[AC, lags] = xcorr(p);
+for i = lag
+	sum = 0;
+	count = 0;
+	for j = 1:length(t)-i
+		sum = sum + p(j)*p(j+i);
+		count = count + 1;
+	end
+	auto_correlation(length(t)+i) = auto_correlation(length(t)-i) = sum/count;
+end
 
-plot(lags.*dt,AC); %plot
+dt = t(2) - t(1);
+plot((1-length(t):length(t)-1).*dt,auto_correlation);
