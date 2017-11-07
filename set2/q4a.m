8cf89d12b1ef12293328332fc81029bb8eaa7762
Abin Thomas <abinthomasonline@gmail.com>
2017-11-07 16:01:08 +0530

    Update q4a.m

--- set2/q4a.m
+++ set2/q4a.m
@@ -1,11 +1,21 @@
-load elcentro.mat %load elcentro data
+load elcentro.mat
 
-AC = xcorr(p); %autocorrelation
+lag = 0:length(t)-1;
+auto_correlation = zeros(1,2*length(lag)-1);
 
-dt = t(2)-t(1); f = 1/dt;
+for i = lag
+	sum = 0;
+	count = 0;
+	for j = 1:length(t)-i
+		sum = sum + p(j)*p(j+i);
+		count = count + 1;
+	end
+	auto_correlation(length(t)+i) = auto_correlation(length(t)-i) = sum/count;
+end
 
-S = abs(fft(AC)); %absolute value of discrete fourier transform of AC
+f = 1/(t(2)-t(1));
+L = length(auto_correlation);
+S = abs(fftshift(fft(auto_correlation))/L);
+w = (2*pi*f/L)*(-L/2:L/2-1);
 
-w = -f/2:f/length(AC):f/2-f/length(AC); %frequency axis
-
-plot(2*pi*w, S); %plot
+plot(w, S);
