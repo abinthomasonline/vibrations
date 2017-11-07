3829cdddca36053d5a9c100d907cd6c9d714b253
Abin Thomas <abinthomasonline@gmail.com>
2017-11-07 22:05:09 +0530

    Update q5b.m

--- set2/q5b.m
+++ set2/q5b.m
@@ -1,20 +1,22 @@
-load elcentro.mat %load elcentro data
+load elcentro.mat
 
-k = 400;
 m = 100;
-zeta = 0.02; %damping ratio
+k = 400;
+zeta = 0.02;
 wn = sqrt(k/m);
 c = 2*m*wn*zeta;
+L = length(t);
+dt = t(2)-t(1);
+dw = (2*pi)/(dt*L);
+w = dw*(-L/2:L/2-1);
 
-dt = t(2)-t(1); f = 1/dt;
+y = fftshift(fft(p))/L;
+S = y.*conj(y)./(2*dw);
 
-S = fft(p).^2; %square of discrete fourier transform of p
+H = 1./(-m*w.^2 + i*c*w + k);
 
-w = 0:f/length(p):f-f/length(p); %frequency axis
-w = 2*pi*w;
-H = 1./((k-m.*w.*w).^2 + (c.*w).^2); %transfer function square
+S_u = S.*H.*conj(H);
 
-w = -f/2:f/length(p):f/2-f/length(p); %frequency axis
-w = 2*pi*w;
-Su = H.*S;
-plot(w, abs(fft(real(ifft(Su)))));
+plot(w, 2*S_u);
+xlabel('w - angular frequency');
+ylabel('PSDF');
