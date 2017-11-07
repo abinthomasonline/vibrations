b9e37046f54857870a635ea02e9cf1fe40a9cafc
Abin Thomas <abinthomasonline@gmail.com>
2017-11-07 20:41:53 +0530

    Update q5a.m

--- set2/q5a.m
+++ set2/q5a.m
@@ -1,16 +1,18 @@
-load elcentro.mat %load elcentro data
+load elcentro.mat
 
-k = 400;
 m = 100;
-zeta = 0.02; %damping ratio
+k = 400;
+zeta = 0.02;
 wn = sqrt(k/m);
 c = 2*m*wn*zeta;
+L = length(t);
+dt = t(2)-t(1);
+dw = (2*pi)/(dt*L);
+w = dw*(-L/2:L/2-1);
 
-dt = t(2)-t(1); f = 1/dt;
-Fw = fft(p); %discrete fourier transform of p
-w = 0:f/length(p):f-f/length(p); %frequency axis
-w = 2*pi*w;
+FFT_p = fftshift(fft(p));
+H = 1./(-m*w.^2 + i*c*w + k);
+FFT_u = H.*FFT_p;
+u = ifft(fftshift(FFT_u))*L;
 
-H = 1./(k-m*w.^2 + 1i*c*w); %transfer function
-U = H.*Fw;
-plot(t, ifft(U))
+plot(t, u);
