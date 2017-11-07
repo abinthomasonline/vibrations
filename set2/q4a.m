e35aaccd9c9909e92ef9bce0b7594cbeb1c713cc
Abin Thomas <abinthomasonline@gmail.com>
2017-11-07 20:08:02 +0530

    Update q4a.m

--- set2/q4a.m
+++ set2/q4a.m
@@ -1,21 +1,25 @@
 load elcentro.mat
-
-lag = 0:length(t)-1;
-auto_correlation = zeros(1,2*length(lag)-1);
+L = length(t);
+lag = 0:L-1;
+auto_correlation = zeros(1,2*L-1);
 
 for i = lag
 	sum = 0;
 	count = 0;
-	for j = 1:length(t)-i
+	for j = 1:L-i
 		sum = sum + p(j)*p(j+i);
 		count = count + 1;
 	end
-	auto_correlation(length(t)+i) = auto_correlation(length(t)-i) = sum/count;
+	auto_correlation(L+i) = auto_correlation(L-i) = sum/count;
 end
 
-f = 1/(t(2)-t(1));
 L = length(auto_correlation);
 S = abs(fftshift(fft(auto_correlation))/L);
-w = (2*pi*f/L)*(-L/2:L/2-1);
+
+dt = t(2)-t(1);
+dw = (2*pi)/(dt*L);
+w = dw*(-L/2:L/2-1);
 
 plot(w, S);
+xlabel('w - angular frequency');
+ylabel('PSDF');
